bf3052b7a1d834fb0f43f401ffe9ad70f5daddd2
j52546 <j52546@gmail.com>
2017-11-16 21:56:36 -0200

    Criada regressão linear

--- Trabalho Aline/startbootstrap-freelancer-gh-pages/js/table.js
+++ Trabalho Aline/startbootstrap-freelancer-gh-pages/js/table.js
@@ -12,3 +12,85 @@
            e.preventDefault();
         }
  
+ $(function(){
+    $('.myBtn').on('click', function(){
+        let qtd = $('.entrada').val()
+        if(qtd !== '' && qtd <= 10 && qtd > 0){
+        for(let i = 0; i < qtd; i++) {
+            let html = `<tr>
+                <td class="xi">
+                    <input class="form-control" type="number"/>
+                </td>
+                <td class="yi">
+                    <input class="form-control" type="number"/>
+                 </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+                <td>
+                
+                </td>
+            </tr>`
+            $('.body-table').append(html)
+        }
+    } else {
+        alert('Quantidade de coluna inválida')
+    }
+    })
+
+
+    $('.entrada').on('keyup', function(e) {
+        if(e.keyCode == 13) {
+            let qtd = $('.entrada').val()
+            if(qtd !== '' && qtd <= 10 && qtd > 0){
+            for(let i = 0; i < qtd; i++) {
+                let html = `<tr>
+                    <td class="xi">
+                        <input class="form-control" type="number"/>
+                    </td>
+                    <td class="yi">
+                        <input class="form-control" type="number"/>
+                     </td>
+                    <td class="xifi" id="xifi${i}">
+    
+                    </td>
+                    <td class="xi2" id="xi2${i}">
+    
+                    </td>
+                    <td class="yi2" id="yi2${i}">
+                    
+                    </td>
+                </tr>`
+                $('.body-table').append(html)
+            }
+        } else {
+            alert('Quantidade de coluna inválida')
+        }
+        }
+    })
+
+    $('.btnCalcular').on('click', function() {
+        let xiElement = $('.xi input')
+        let yiElement = $('.yi input')
+        let xi = Array()
+        let yi = Array()
+        for(let i = 0; i < $('.xi input').length; i++) {
+            xi.push(xiElement[i].value)
+            yi.push(yiElement[i].value)
+        }
+       for(let i = 0;i < xi.length; i++) {
+           let xifi = `<span>${xi[i] * yi[i]}</span>`
+           let xi2 = `<span>${Math.pow(xi[i], 2)}</span>`
+           let yi2 = `<span>${Math.pow(yi[i], 2)}</span>`
+           $('#xifi' + i ).html(xifi)
+           $('#xi2' + i ).html(xi2)
+           $('#yi2' + i ).html(yi2)
+           
+           
+       }
+
+    })
+ })
