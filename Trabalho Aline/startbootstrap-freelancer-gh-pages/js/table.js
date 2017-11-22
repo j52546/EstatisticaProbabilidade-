15d7fb7feb9d2fc0244c25fcc56de43cc4681133
BrunoCBarbosa <bruno.cbarbosa90@gmail.com>
2017-11-22 20:56:51 -0200

    tentativa de cont

--- Trabalho Aline/startbootstrap-freelancer-gh-pages/js/table.js
+++ Trabalho Aline/startbootstrap-freelancer-gh-pages/js/table.js
@@ -77,19 +77,34 @@
         let yiElement = $('.yi input')
         let xi = Array()
         let yi = Array()
+        let html = `<tr>
+            <td class="sXi" id="sXi">
+            
+            </td>
+            <td class="sXi" id="sYi"></td>
+            <td class="sXiYi" id="sXiYi"></td>
+             <td class="sXi2" id="sXi2"></td>
+                <td class="sYi2" id="sYi2"></td>
+            </tr>`
+            $('.body-table').append(html)
         for(let i = 0; i < $('.xi input').length; i++) {
             xi.push(xiElement[i].value)
             yi.push(yiElement[i].value)
         }
+        let cont=0;
        for(let i = 0;i < xi.length; i++) {
            let xifi = `<span>${xi[i] * yi[i]}</span>`
            let xi2 = `<span>${Math.pow(xi[i], 2)}</span>`
            let yi2 = `<span>${Math.pow(yi[i], 2)}</span>`
+           cont += `<span>${xi[i]}</span>`
            $('#xifi' + i ).html(xifi)
            $('#xi2' + i ).html(xi2)
            $('#yi2' + i ).html(yi2)
        }
+       let somaXi = `<span>${cont}</span>`
+       $('#sXi').html(somaXi)
+    })
+   
     })
     
-  
-    })
+    
